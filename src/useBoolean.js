1b118f8f8bc7687380bd510438e2cc29d2f83bd5
吳俊緯 <wcw850710@gmail.com>
2021-03-07 20:52:09 +0800

    useBoolean: 用 useMethods 改寫

--- src/useBoolean.js
+++ src/useBoolean.js
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import useMethods from './useMethods'
 
 function useBoolean(initialValue = false) {
-  const [bool, setBool] = useState(initialValue)
-  const setTrue = () => setBool(true)
-  const setFalse = () => setBool(false)
-  const toggle = () => setBool(e => !e)
-  return [bool, { setTrue, setFalse, toggle }]
+	const [bool, methods] = useMethods(initialValue, {
+		setTrue: () => true,
+		setFalse: () => false,
+		toggle: state => !state,
+	})
+	return [bool, methods]
 }
 
 export default useBoolean
