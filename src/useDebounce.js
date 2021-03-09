215c3d9ccf4173e6e218a9e952332d8c23e6e5b0
吳俊緯 <wcw850710@gmail.com>
2021-03-09 10:13:21 +0800

    useDebounce: add jsdoc

--- src/useDebounce.js
+++ src/useDebounce.js
@@ -1,6 +1,12 @@
 import { useRef, useCallback } from 'react'
 import timeout from 'l8-lib'
 
+/**
+ * 防抖鉤子
+ * @param {function(...*)} fun
+ * @param {number} [delay=500] delay
+ * @returns {function(...*): void}
+ */
 function useDebounce(fun, delay = 500) {
 	const timer = useRef(timeout())
 
